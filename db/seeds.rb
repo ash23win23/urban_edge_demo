01ebf9a1a74c1807d168c30593bc8bad8d1ecc56
ash23win96 <theyelllowgamer@gmail.com>
2023-11-22 16:59:46 +0530

    Added Faker gem to populate the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+# Clear existing data
+Product.destroy_all
+Category.destroy_all
+User.destroy_all
+
+# Create a user
+user = User.create!(
+  email: 'user@example.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+# Seed categories
+categories = ['Electronics', 'Clothing', 'Books', 'Toys', 'Home Goods']
+
+categories.each do |category_name|
+  Category.create!(name: category_name)
+end
+
+# Seed products with associated categories and user
+10.times do
+  category = Category.all.sample
+  Product.create!(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.sentence,
+    price: Faker::Commerce.price(range: 10..100.0, as_string: true),
+    category: category,
+    user: user
+  )
+end
