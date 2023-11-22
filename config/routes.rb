c563851997f469bb70f3761549954fa13f2ee989
ash23win96 <theyelllowgamer@gmail.com>
2023-11-22 15:41:11 +0530

    Added Views and Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "store#index"
+  resources :products
+  resources :categories
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_093159) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_094157) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_093159) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
@@ -39,5 +41,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_093159) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
 end
